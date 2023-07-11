b3b03f746a06dcb60d1af4c7277eb0b57a3ba53a
Sachin25sd <76631128+Sachin25sd@users.noreply.github.com>
2023-07-11 15:35:03 +1000

    Updated TC-1
    Updated TC-1

--- tests/test-1.spec.js
+++ tests/test-1.spec.js
@@ -4,11 +4,12 @@ const TestData = require("../DemoQA - TestData/testData");
 
 test("Login Test", async ({ page }) => {
   const loginPage = new LoginPage(page);
-  //Login to website
+  //Step 1: Navigate to DemoQA website
   await test.step("Navigate to DemoQA", async () => {
     try {
       await page.goto("https://demoqa.com/login", { timeout: 30000 });
     } catch (error) {
+      // Report the error if the page load time exceeds the timeout
       report.expectStatus({
         status: "Failed",
         message: "Page load time exceeded 30 seconds timeout",
@@ -18,21 +19,24 @@ test("Login Test", async ({ page }) => {
     }
   });
 
-  // Invalid Login attempt
+  // Step 2: Invalid Login attempt
   await test.step("Invalid Test", async () => {
+    // Fill in invalid username and password
     await loginPage.fillUsername(TestData.invalidUsername);
     await loginPage.fillPassword(TestData.invalidPassword);
     await loginPage.clickLoginButton();
+    // Assertion: Check if the error message is displayed
     expect(await loginPage.getErrorMessage()).toBe(
       "Invalid username or password!"
     );
   });
 
-  // Valid login attempt
+  // Step 3: Valid login attempt
   await test.step("Valid Test", async () => {
     await loginPage.fillUsername(TestData.validUsername);
     await loginPage.fillPassword(TestData.validPassword);
     await loginPage.clickLoginButton();
+    //Assertion: Logout option is visible after successful login
     await loginPage.clickLogoutButton();
   });
 });
